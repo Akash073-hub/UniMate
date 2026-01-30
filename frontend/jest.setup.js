81412d15a74bd769433a7393a9090dad445a6439
Akash bisht <aayush45r@gmail.com>
2026-01-30 19:40:46 +0530

    fixed all the dependecies issues

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,21 +1,12 @@
 // jest.setup.js
 
-// Mock react-native modules that don't work well in Jest
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
-
-// Silence the warning: Animated: `useNativeDriver` is not supported
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper', () => {
-  return {
-    __esModule: true,
-    default: {
-      API: {},
-    },
-  };
-});
-
-// Mock AsyncStorage if you're using it
-jest.mock('@react-native-async-storage/async-storage', () =>
-  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
-);
-
+// Mock fetch
 global.fetch = jest.fn();
+
+// Suppress console logs during tests (optional)
+global.console = {
+  ...console,
+  log: jest.fn(),
+  debug: jest.fn(),
+  info: jest.fn(),
+};
